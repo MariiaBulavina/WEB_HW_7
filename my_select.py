21dc9ac1ec43f72342ec1e98ad2ec9b04a727d4f
Mariia Bulavina <120931054+MariiaBulavina@users.noreply.github.com>
2024-01-22 09:45:33 -0600

    1

---
+++ my_select.py
@@ -0,0 +1,41 @@
+
+
+
+def select_1():
+    ...
+
+
+def select_2():
+    ...
+
+
+def select_3():
+    ...
+
+
+def select_4():
+    ...
+
+
+def select_5():
+    ...
+
+
+def select_6():
+    ...
+
+
+def select_7():
+    ...
+
+
+def select_8():
+    ...
+
+
+def select_9():
+    ...
+
+
+def select_10():
+    ...
